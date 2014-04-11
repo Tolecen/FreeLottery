74a0dd5ab3518c80f0be7f36e1541f7789c52db1
wangxr <wangxr1990123@126.com>
2014-04-11 10:57:38 +0800

    多余资源删除

---
+++ Classes/UserCenter/MessageCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageCenterViewController.h
+//  Boyacai
+//
+//  Created by wangxr on 14-4-11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageCenterViewController : UIViewController
+
+@end
