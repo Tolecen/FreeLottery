963ec366e6fcfd351876719e21d984c060aec7aa
wangxr <wangxr1990123@126.com>
2014-06-12 15:52:50 +0800

    .

--- Classes/InviteViewController.m
+++ Classes/InviteViewController.m
@@ -105,31 +105,32 @@
     [scrollView addSubview:label2];
     label2.text = @"邀请人奖励";
     
-    self.inviteAwardL = [[UILabel alloc]initWithFrame:CGRectMake(10, 140, 310,20)];
+    self.inviteAwardL = [[UILabel alloc]initWithFrame:CGRectMake(10, 140, 310,30)];
+    _inviteAwardL.numberOfLines = 0;
     [scrollView addSubview:_inviteAwardL];
     _inviteAwardL.textColor = [UIColor grayColor];
     _inviteAwardL.font = [UIFont systemFontOfSize:12];
     
-    UILabel * label4 = [[UILabel alloc]initWithFrame:CGRectMake(10, 170, 310,20)];
+    UILabel * label4 = [[UILabel alloc]initWithFrame:CGRectMake(10, 180, 310,20)];
     [scrollView addSubview:label4];
     label4.textColor = [UIColor grayColor];
     label4.font = [UIFont systemFontOfSize:15];
     label4.text = @"Boss模式奖励";
     
-    UIImageView * image2 = [[UIImageView alloc]initWithFrame:CGRectMake(110, 172, 18, 18)];
+    UIImageView * image2 = [[UIImageView alloc]initWithFrame:CGRectMake(110, 182, 18, 18)];
     image2.image = [UIImage imageNamed:@"BOSS"];
     [scrollView addSubview:image2];
     
     UIButton * bossB = [UIButton buttonWithType:UIButtonTypeCustom];
-    bossB.frame = CGRectMake(10, 170, 130, 20);
+    bossB.frame = CGRectMake(10, 175, 140, 30);
     [scrollView addSubview:bossB];
     [bossB addTarget:self action:@selector(showBossTaskInviter) forControlEvents:UIControlEventTouchUpInside];
     
-    self.progressImg= [[UIImageView alloc]initWithFrame:CGRectMake(10, 200, 293.5, 31)];
+    self.progressImg= [[UIImageView alloc]initWithFrame:CGRectMake(10, 210, 293.5, 31)];
     _progressImg.image = [UIImage imageNamed:@"jindudi"];
     [scrollView addSubview:_progressImg];
     
-    self.percentL = [[UILabel alloc]initWithFrame:CGRectMake(10, 205, 100,20)];
+    self.percentL = [[UILabel alloc]initWithFrame:CGRectMake(10, 215, 100,20)];
     [scrollView addSubview:_percentL];
     _percentL.textColor = [UIColor whiteColor];
     _percentL.backgroundColor = [UIColor clearColor];
@@ -137,69 +138,69 @@
     _percentL.font = [UIFont systemFontOfSize:15];
     _percentL.text = @"未完成";
     
-    UILabel * label3 = [[UILabel alloc]initWithFrame:CGRectMake(10, 240, 20, 20)];
+    UILabel * label3 = [[UILabel alloc]initWithFrame:CGRectMake(10, 250, 20, 20)];
     [scrollView addSubview:label3];
     label3.textColor = [UIColor grayColor];
     label3.font = [UIFont systemFontOfSize:15];
     label3.tag = 10;
-    UILabel * label5 = [[UILabel alloc]initWithFrame:CGRectMake(100, 240, 20, 20)];
+    UILabel * label5 = [[UILabel alloc]initWithFrame:CGRectMake(100, 250, 20, 20)];
     [scrollView addSubview:label5];
     label5.textColor = [UIColor grayColor];
     label5.font = [UIFont systemFontOfSize:15];
     label5.tag = 11;
-    UILabel * label7 = [[UILabel alloc]initWithFrame:CGRectMake(200, 240, 20, 20)];
+    UILabel * label7 = [[UILabel alloc]initWithFrame:CGRectMake(200, 250, 20, 20)];
     [scrollView addSubview:label7];
     label7.textColor = [UIColor grayColor];
     label7.font = [UIFont systemFontOfSize:15];
     label7.tag = 12;
     
-    UIImageView * image4 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 270, 18, 18)];
+    UIImageView * image4 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 280, 18, 18)];
     image4.image = [UIImage imageNamed:@"shouyaoqing"];
     [scrollView addSubview:image4];
     
-    UILabel * label6 = [[UILabel alloc]initWithFrame:CGRectMake(40, 270, 100, 20)];
+    UILabel * label6 = [[UILabel alloc]initWithFrame:CGRectMake(40, 280, 100, 20)];
     [scrollView addSubview:label6];
     label6.text = @"受邀人奖励";
     
-    self.invitedAwardL = [[UILabel alloc]initWithFrame:CGRectMake(10, 300, 310,20)];
+    self.invitedAwardL = [[UILabel alloc]initWithFrame:CGRectMake(10, 310, 310,20)];
     [scrollView addSubview:_invitedAwardL];
     _invitedAwardL.textColor = [UIColor grayColor];
     _invitedAwardL.font = [UIFont systemFontOfSize:12];
     
-    UIView * view2 = [[UIView alloc]initWithFrame:CGRectMake(0, 330, self.view.frame.size.width, 170)];
+    UIView * view2 = [[UIView alloc]initWithFrame:CGRectMake(0, 340, self.view.frame.size.width, 170)];
     view2.backgroundColor = [ColorUtils parseColorFromRGB:@"#efede9"];
     [scrollView addSubview:view2];
     
-    UIImageView * image5 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 340, 18, 18)];
+    UIImageView * image5 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 350, 18, 18)];
     image5.image = [UIImage imageNamed:@"liucheng"];
     [scrollView addSubview:image5];
     
-    UILabel * label8 = [[UILabel alloc]initWithFrame:CGRectMake(40, 340, 100, 20)];
+    UILabel * label8 = [[UILabel alloc]initWithFrame:CGRectMake(40, 350, 100, 20)];
     [scrollView addSubview:label8];
     label8.backgroundColor = [UIColor clearColor];
     label8.text = @"推广流程";
     
-    UIImageView * image6 = [[UIImageView alloc]initWithFrame:CGRectMake(15, 370, 288, 89)];
+    UIImageView * image6 = [[UIImageView alloc]initWithFrame:CGRectMake(15, 380, 288, 89)];
     image6.image = [UIImage imageNamed:@"liuchengtu"];
     [scrollView addSubview:image6];
     
-    UILabel * label9 = [[UILabel alloc]initWithFrame:CGRectMake(10, 470, 310,20)];
+    UILabel * label9 = [[UILabel alloc]initWithFrame:CGRectMake(10, 480, 310,20)];
     [scrollView addSubview:label9];
     label9.textColor = [UIColor redColor];
     label9.backgroundColor = [UIColor clearColor];
     label9.font = [UIFont systemFontOfSize:12];
     label9.text = @"温馨提示:一定让好友在注册时填入你的邀请码";
     
-    UIImageView * image7 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 510, 18, 18)];
+    UIImageView * image7 = [[UIImageView alloc]initWithFrame:CGRectMake(10, 520, 18, 18)];
     image7.image = [UIImage imageNamed:@"tuguang"];
     [scrollView addSubview:image7];
     
-    UILabel * label10 = [[UILabel alloc]initWithFrame:CGRectMake(40, 510, 100, 20)];
+    UILabel * label10 = [[UILabel alloc]initWithFrame:CGRectMake(40, 520, 100, 20)];
     [scrollView addSubview:label10];
     label10.text = @"推广方式";
     
     UIButton * shareB = [UIButton buttonWithType:UIButtonTypeCustom];
-    shareB.frame = CGRectMake(15, 540, 291, 38);
+    shareB.frame = CGRectMake(15, 550, 291, 38);
     [shareB setBackgroundImage:[UIImage imageNamed:@"huisebtn"] forState:UIControlStateNormal];
     [shareB setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [shareB setTitle:@"分享" forState:UIControlStateNormal];
@@ -207,7 +208,7 @@
     [shareB addTarget:self action:@selector(shareInviteCode) forControlEvents:UIControlEventTouchUpInside];
     
     UIButton * addressB = [UIButton buttonWithType:UIButtonTypeCustom];
-    addressB.frame = CGRectMake(15, 590, 291, 38);
+    addressB.frame = CGRectMake(15, 600, 291, 38);
     [addressB setBackgroundImage:[UIImage imageNamed:@"huisebtn"] forState:UIControlStateNormal];
     [addressB setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [addressB setTitle:@"一键安装地址获取" forState:UIControlStateNormal];
@@ -215,7 +216,7 @@
     [addressB addTarget:self action:@selector(copyDownLoadAddress) forControlEvents:UIControlEventTouchUpInside];
     
     UIButton * twoDimensionB = [UIButton buttonWithType:UIButtonTypeCustom];
-    twoDimensionB.frame = CGRectMake(15, 640, 291, 38);
+    twoDimensionB.frame = CGRectMake(15, 650, 291, 38);
     [twoDimensionB setBackgroundImage:[UIImage imageNamed:@"huisebtn"] forState:UIControlStateNormal];
     [twoDimensionB setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [twoDimensionB setTitle:@"二维码扫描安装" forState:UIControlStateNormal];
